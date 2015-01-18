6f7e92d8d411e9c0c2848e0b6bbb02a359200523
Yusuf Azman <stylo@umich.edu>
2015-01-18 15:28:12 -0500

    make it more instagram-like

--- angularjs/app.js
+++ angularjs/app.js
@@ -1,5 +1,5 @@
 angular.module("SocialFeeds", [])
-.constant("instagramUrl", "https://api.instagram.com/v1/tags/gopro/media/recent?client_id=2aafa51114a14efda303e22ffa48c6e2&callback=JSON_CALLBACK")
+.constant("instagramUrl", "https://api.instagram.com/v1/tags/green/media/recent?client_id=2aafa51114a14efda303e22ffa48c6e2&callback=JSON_CALLBACK")
 .controller("InstagramCtrl", function($scope, $http, instagramUrl) {
 	$scope.pics = [];
 	$scope.getPics = function() {
